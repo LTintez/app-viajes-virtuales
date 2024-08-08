4fd4a851a69385e64419e18120e0e1dfb392cbf8
Lautaro Tintez <ltintez@asap-consulting.net>
2024-08-08 13:06:44 -0300

    cards home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'   
 import HeroComponent from '../components/HeroComponent'
 import MainSection from '../components/MainSection'
+import FeaturedSection from '../components/FeaturedSection'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       < HeroComponent/>
       < MainSection/>
+      < FeaturedSection/>
     </div>
   )
 }
